90c8e2e6797c5a0f6386b88d81fe05a068278560
sajithaliyanage <sajithaliyanage@gmail.com>
2022-01-22 16:50:36 +0300

    Update cibr cluster values

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -19,17 +19,25 @@ if [ $BITCOIND_STATUS -ne 0 ]; then
   fi
 fi
 
+#psql -h "$GREENPLUM_SERVICE_HOST" -p "$GREENPLUM_SERVICE_PORT" --user=gpadmin -f blockstack_schema.sql
+
 export last_block=0
 while sleep 1; do
-  block_count="$(bitcoin-cli -rpcuser=user -rpcpassword=password -rpcport=8332 getblockcount)"
-  if ((block_count > ((last_block + 15)))); then
-    echo "Processing start for block range $((last_block + 1))-$((last_block + 10))"
-    bitcoinetl export_blocks_and_transactions --start-block "$((last_block + 1))" --end-block "$((last_block + 10))" \
-      --provider-uri $PROVIDER_URI --chain bitcoin --blocks-output blocks.json --transactions-output transactions.json &&
-    bitcoinetl enrich_transactions --provider-uri $PROVIDER_URI --transactions-input transactions.json \
-      --transactions-output enriched_transactions.json &&
-    python3 bitcoin_to_rabbitmq_exporter.py &&
-    purge_data &&
-    export last_block=$((last_block + 10))
-  fi
+    block_count="$(bitcoin-cli -rpcuser=user -rpcpassword=password -rpcport=8332 getblockcount)"
+    if (( block_count > ((last_block+105)) )); then
+        echo "Processing start for block range $((last_block+1))-$((last_block+100))"
+        bitcoinetl export_blocks_and_transactions --start-block "$((last_block+1))" --end-block "$((last_block+100))" \
+        --provider-uri $PROVIDER_URI --chain bitcoin --blocks-output blocks.json --transactions-output transactions.json && \
+        bitcoinetl enrich_transactions --provider-uri $PROVIDER_URI --transactions-input transactions.json \
+        --transactions-output enriched_transactions.json  && \
+        python3 chain_formatter.py && \
+        #psql -h "$GREENPLUM_SERVICE_HOST" -p "$GREENPLUM_SERVICE_PORT" -d blockstack --user=gpadmin -c "\\COPY btc_block(height, hash, block_time, tx_count) FROM blocks_sql.csv CSV DELIMITER E','"
+        #psql -h "$GREENPLUM_SERVICE_HOST" -p "$GREENPLUM_SERVICE_PORT" -d blockstack --user=gpadmin -c "\\COPY btc_transaction(hash, block_number, index, fee, input_value, output_value, is_coinbase, input_count, output_count) FROM tx_sql.csv CSV DELIMITER E','"
+        #psql -h "$GREENPLUM_SERVICE_HOST" -p "$GREENPLUM_SERVICE_PORT" -d blockstack --user=gpadmin -c "\\COPY btc_input_address(tx_hash, address, address_type, tx_value) FROM in_addr_sql.csv CSV DELIMITER E','"
+        #psql -h "$GREENPLUM_SERVICE_HOST" -p "$GREENPLUM_SERVICE_PORT" -d blockstack --user=gpadmin -c "\\COPY btc_output_address(tx_hash, address, address_type, tx_value) FROM out_addr_sql.csv CSV DELIMITER E','"
+        purge_data
+        export last_block=$((last_block+100))
+    fi
 done
+
+echo "Ending bitcoin parser..."
